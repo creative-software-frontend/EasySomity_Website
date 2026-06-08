ab13a2509449261e6dfa241150c88d40441cbfe6
Dilouar Hossain <dilouarbd@gmail.com>
2026-06-08 14:36:08 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Navbar from '@/components/Navbar';
+import HeroSection from '@/components/HeroSection';
+import ModulesSection from '@/components/ModulesSection';
+import PricingSection from '@/components/PricingSection';
+import ClientsSection from '@/components/ClientsSection';
+import ContactSection from '@/components/ContactSection';
+import TermsSection from '@/components/TermsSection';
+import RelatedProjects from '@/components/RelatedProjects';
+import Footer from '@/components/Footer';
+import DemoSection from '@/components/DemoSection';
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <HeroSection />
+      <DemoSection />
+      <ModulesSection />
+      <PricingSection />
+      <RelatedProjects />
+      <ClientsSection />
+      <ContactSection />
+      <TermsSection />
+      <Footer />
+    </main>
+  );
+}
